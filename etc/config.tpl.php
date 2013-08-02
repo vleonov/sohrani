3d0f4dda215d6d01daca03d4f94a7bf08e2b52e5
Vitali Leonov <leonov.me@gmail.com>
2013-08-02 23:40:24 +0400

    raw repo fix

---
+++ etc/config.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+    'db' => array(
+        'host' => 'localhost',
+        'name' => 'sohrani',
+        'user' => 'sohrani',
+        'password' => '',
+    ),
+    'auth' => array(
+        'gAuth' => array(
+            'clientId' => '%clientId%',
+            'clientSecret' => '%clientSecret%',
+            'developerKey' => '%developerKey%',
+        ),
+        'login' => 'leonov.me@gmail.com',
+    ),
+    'base' => array(
+	'vleonov.me' => '/sohrani',
+	'sohrani.info' => '',
+    ),
+);
